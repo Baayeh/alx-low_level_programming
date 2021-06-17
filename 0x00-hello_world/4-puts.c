e5f1f11f3119bd4c39bfb62d511ca8c49b437122
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-17 06:08:17 -0700

    Adding file again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char MyPuts[] = "\"Programming is like building a multilingual puzzle\n";
+	char MyPuts[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(MyPuts);
 	return (0);
