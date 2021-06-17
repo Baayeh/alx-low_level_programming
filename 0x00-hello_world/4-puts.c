f93424afa66b4c69661da6fc9a92ea2fc8ccceea
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-17 05:56:57 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int main() {
-	char MyPuts[] = "Programming is like building a multilingual puzzle\n";
+int main()
+{
+	char MyPuts[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(MyPuts);
 	return(0);
 }
