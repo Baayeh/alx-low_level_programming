edcf8888b76bffc5b225bef1eecc453c13691b9d
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-18 04:10:47 -0700

    Printing lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	for (j = 65; j <= 90; j++)
+	{
+		putchar(j);
+	}
+	return (0);
+}
