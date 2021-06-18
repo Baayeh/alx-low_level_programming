d287b3a41c7cef29f6ab8a63fbb8d3bf0006e839
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-18 05:46:05 -0700

    Printing numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
