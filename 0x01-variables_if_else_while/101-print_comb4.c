8553aa7dc7d425087a0f7bc3f86a24c845609e54
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-18 09:03:48 -0700

    file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <='9'; k++)
+			{
+				if (j > i && k > j)
+				{
+					if (i == '0' && j == '1' && k == '2')
+					{
+						write(1, "0", 1);
+						write(1, "1", 1);
+						write(1, "2", 2);
+					} else
+					{
+						putchar(',');
+						putchar(' ');
+						putchar(i);
+						putchar(j);
+						putchar(k);
+					}
+				}
+			}
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
