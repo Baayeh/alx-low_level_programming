0a3a41c3085af228e5210e2c5e3afd8910d4ca60
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-23 07:01:26 -0700

    Adding file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10(void) - Printing the alphabets 10 times
+ *
+ * Return: 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i, counter;
+
+	counter = 0;
+
+	while (counter < 10)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		counter++;
+		_putchar('\n');
+	}
+}
