174742800c4711c8c4b782480248a4d847af2bfc
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-23 07:20:07 -0700

    Add file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * isaplha - checks alphabetic letter
+ * @c: single letter
+ * Reurn: 1 if c is a letter, else 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 197) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
