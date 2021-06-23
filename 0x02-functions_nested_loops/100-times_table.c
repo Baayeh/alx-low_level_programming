2b6b5b7b9d237bb59cd7e21c71eedcb5c98a5c0f
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-23 16:23:24 +0000

    Adding files

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,33 @@
  */
 void print_times_table(int n)
 {
+    int r, c, d;
 
+	for (r = 0; r <= n; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= n; c++)
+		{
+			d = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 	return (0);
 }
