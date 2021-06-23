906cc8859caa3c5fb7a2160b33f99ed80a537ed8
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-23 14:36:36 +0000

    Adding new file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * print_sign - prints the sign of a number
- * @n: number
- * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
- */
+* print_sign - prints the sign of a number
+* @n: number
+* Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
+*/
 int print_sign(int n)
 {
 	if (n > 0)
