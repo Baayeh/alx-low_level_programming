ba62b173641ea0506ee6e5e38cc3189165f59cb8
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-23 07:09:07 -0700

    Add file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _islower - check for lowercase
+ *
+ * Return: 1 if c is lowercase, else 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 197)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
