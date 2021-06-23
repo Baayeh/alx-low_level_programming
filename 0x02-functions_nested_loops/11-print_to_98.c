2377ea976430f60d9def53a8c50ee1bc87ec507a
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-23 08:45:20 -0700

    Adding file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 - print all natural numbers from n to 98
  * user input's number prints to 98, regardless < 98 or > 98
- * @n: number
+ * @n: numbier
  * Return: Always 0
  */
 void print_to_98(int n)
