9093e5327c7f1ad08eecce95c83835685eb2591a
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-25 03:56:54 -0700

    most num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * _putchar only 3 times
+ * _putchar - use only 3 times
  * Return: 0-14 x10 followed by new line
  */
 void more_numbers(void)
