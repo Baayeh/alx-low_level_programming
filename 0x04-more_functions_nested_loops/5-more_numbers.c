a23a458811d443e313d4d1c8ee9624aa455fd598
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-25 10:55:34 +0000

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * _putchar only 3 times
+ * Return: 0-14 x10 followed by new line
+ */
+void more_numbers(void)
+{
+	int i, r;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
