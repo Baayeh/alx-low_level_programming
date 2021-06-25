1f1b5afa3d82ed7a5579dd0401f72dbd51a1c166
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-25 03:52:58 -0700

    Print most num

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
-
 /**
  * print_most_numbers - prints the numbers, from 0 to 9
  *
- * Return: 0-9 excluding 2 and 4
+ * Return: 0-9 excluding 2 and 4 followed by new line
  */
 void print_most_numbers(void)
 {
@@ -11,7 +10,7 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if ((i == 2) || (i == 4))
+		if (i == 2 || i == 4)
 			continue;
 		else
 			_putchar(i + '0');
