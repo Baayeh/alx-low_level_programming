8ad850cf676eb3d12174361991842356be5541bb
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-06-28 05:27:06 -0700

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * strlen - Return the length of the string
+ * @str: string
+ *
+ * Return: length
+ */
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
